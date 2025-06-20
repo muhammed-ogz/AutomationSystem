fab3449603ca47c110eeadeee4759ef043053c7e
muhammedogz_ <muhammednabioguz@gmail.com>
2025-06-20 14:02:10 +0300

    backend infrastructure is being established. part2

--- api-server/src/index.ts
+++
@@ -1,5 +0,0 @@
-import cors from 'cors';
-import express from 'express';
-import dotenv from 'dotenv';
-import {connect} from 'mongoose';
-import pino from 'pino';
---
+++ services/api-server/src/index.ts
@@ -0,0 +1,44 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { connect } from "mongoose";
+import path from "path";
+import pino from "pino";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+// controllers are connected here
+
+//middlewares are connected here
+const app = express();
+const logger = pino();
+const PORT = 5000;
+
+app.set("x-powered-by", false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/public", express.static(path.join(__dirname, "../public")));
+
+(async function () {
+  try {
+    logger.info("Connecting to MongoDB...");
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is not defined in .env file");
+    }
+    await connect(process.env.MONGODB_URI);
+    logger.info("Connected to MongoDB successfully");
+    app.listen(PORT, "0.0.0.0", () => {
+      logger.info(`Server is running on http://0.0.0.0:${PORT}`);
+    });
+  } catch (err: any) {
+    logger.error(
+      `An error occurred during application startup : ${err.message || err}.`
+    );
+  }
+})();
